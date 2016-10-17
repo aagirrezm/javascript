cb3d19de9d4634e0dd87fc64e5ea0a84c491aaac
zmwebdev <zmwebdev@gmail.com>
2016-10-17 11:56:19 +0000

    m

---
+++ 00-HelloWorld/grammarandtypes.js
@@ -0,0 +1,21 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types
+
+/**
+ * Example 1
+ */
+var x;
+console.log(x === undefined); // true
+x = 3;
+ 
+/**
+ * Example 2
+ */
+var myvar = "my value";
+ 
+(function() {
+  var myvar;
+  console.log(myvar); // undefined
+  myvar = "local value";
+})();
+
+console.log(window.myvar);
